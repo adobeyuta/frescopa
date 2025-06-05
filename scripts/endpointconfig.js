691f9955eecb19537bdbb2e736d12b04f09028a8
adobeyuta <154684902+adobeyuta@users.noreply.github.com>
2025-06-05 13:39:38 +0900

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
+  return 'https://publish-p137952-e1400392.adobeaemcloud.com/';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p149891-e1546481.adobeaemcloud.com';
+  return 'https://author-p137952-e1400392.adobeaemcloud.com/';
 }
 
 export { getAEMPublish, getAEMAuthor };
